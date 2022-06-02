ed790fa74c3f3814a331bb4c640fabbd0c6085e9
= <=>
2022-06-02 15:30:46 +0200

    cookies added

--- backend/auth/auth_middleware.py
+++ backend/auth/auth_middleware.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
-from typing import Union
+from typing import Union, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.security import OAuth2
 from sqlalchemy.orm import Session
 from database import get_db
 from db.device import Device
@@ -23,8 +25,36 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     username: Union[str, None] = None
 
+class OAuth2PasswordBearerCookie(OAuth2):
+    def __init__(
+        self,
+        token_url: str,
+        scheme_name: str = None,
+        scopes: dict = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": token_url, "scopes": scopes})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.cookies.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+
+        return param
+
+oauth2_scheme = OAuth2PasswordBearerCookie(token_url="users/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
