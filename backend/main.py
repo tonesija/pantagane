35252b8a3208128a46c9397307a2387c1b3a2bbc
Tonio Ercegović <tonio.ercegovic@fer.hr>
2022-06-02 21:49:26 +0200

    allowed all CORS

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from database import Base, engine
 from iot_platform.mqtt_client import mqtt_subscribe_sensor
 from routers import readings_router, devices_router, users_router
-from settings import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
 from iot_platform.iot_platform_handlers import base_handler
 
@@ -14,6 +14,15 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+# Allow CORS for now
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(readings_router.router)
 app.include_router(devices_router.router)
