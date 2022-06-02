72fef9e264ecfdf9e5f5465f26c2451560cf6cc0
Ivan Vlahov <30372377+vlahovivan@users.noreply.github.com>
2022-06-02 23:29:32 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["set-cookie"],
 )
 
 app.include_router(readings_router.router)
